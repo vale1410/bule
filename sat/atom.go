f280e3ce3bebd34b0d9b93c62adf141bc834d623
vale <vale1410@gmail.com>
2015-01-16 16:50:39 +1100

    refactoring

--- sat/atom.go
+++ sat/atom.go
@@ -1,165 +1,165 @@
 package sat
 
 import (
-    "bufio"
-    "fmt"
-    "os"
-    "strconv"
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
 )
 
 type Pred string
 
 type Atom interface {
-    Id() string
+	Id() string
 }
 
 type AtomP struct {
-    P Pred
+	P Pred
 }
 
 type Atom1 struct {
-    V int
+	V int
 }
 
 type AtomP1 struct {
-    P Pred
-    V int
+	P Pred
+	V int
 }
 
 type AtomP2 struct {
-    P  Pred
-    V1 int
-    V2 int
+	P  Pred
+	V1 int
+	V2 int
 }
 
 type AtomP3 struct {
-    P  Pred
-    V1 int
-    V2 int
-    V3 int
+	P  Pred
+	V1 int
+	V2 int
+	V3 int
 }
 
 func (a Atom1) Id() string {
-    return strconv.Itoa(a.V)
+	return strconv.Itoa(a.V)
 }
 
 func (a AtomP) Id() string {
-    return string(a.P)
+	return string(a.P)
 }
 
 func (a AtomP1) Id() string {
-    return string(a.P) + "(" + strconv.Itoa(a.V) + ")"
+	return string(a.P) + "(" + strconv.Itoa(a.V) + ")"
 }
 
 func (a AtomP2) Id() string {
-    return string(a.P) + "(" + strconv.Itoa(a.V1) + "," + strconv.Itoa(a.V2) + ")"
+	return string(a.P) + "(" + strconv.Itoa(a.V1) + "," + strconv.Itoa(a.V2) + ")"
 }
 
 func (a AtomP3) Id() string {
-    return string(a.P) + "(" + strconv.Itoa(a.V1) + "," + strconv.Itoa(a.V2) + "," + strconv.Itoa(a.V3) + ")"
+	return string(a.P) + "(" + strconv.Itoa(a.V1) + "," + strconv.Itoa(a.V2) + "," + strconv.Itoa(a.V3) + ")"
 }
 
 func NewAtomP(p Pred) Atom {
-    return AtomP{p}
+	return AtomP{p}
 }
 
 func NewAtom1(v int) Atom {
-    return Atom1{v}
+	return Atom1{v}
 }
 
 func NewAtomP1(p Pred, v int) Atom {
-    return AtomP1{p, v}
+	return AtomP1{p, v}
 }
 
 func NewAtomP2(p Pred, v1, v2 int) Atom {
-    return AtomP2{p, v1, v2}
+	return AtomP2{p, v1, v2}
 }
 
 func NewAtomP3(p Pred, v1, v2, v3 int) Atom {
-    return AtomP3{p, v1, v2, v3}
+	return AtomP3{p, v1, v2, v3}
 }
 
 // TODO; once demanded do a generic atom type
 //type AtomPN struct {
 
 type Literal struct {
-    Sign bool
-    A    Atom
+	Sign bool
+	A    Atom
 }
 
 func Neg(l Literal) Literal {
-    l.Sign = !l.Sign
-    return l
+	l.Sign = !l.Sign
+	return l
 }
 
 func (l Literal) ToTxt() (s string) {
-    if !l.Sign {
-        s += "-"
-    } else {
-        s += " "
-    }
-    //s += "x"
-    s += l.A.Id()
-    //s += " "
-    return
+	if !l.Sign {
+		s += "-"
+	} else {
+		s += " "
+	}
+	//s += "x"
+	s += l.A.Id()
+	//s += " "
+	return
 }
 
 func (l Literal) ToTex() (s string) {
-    if !l.Sign {
-        s += "\\bar "
-    }
-    //s += "x_{"
-    s += l.A.Id()
-    //s += "}"
-    return
+	if !l.Sign {
+		s += "\\bar "
+	}
+	//s += "x_{"
+	s += l.A.Id()
+	//s += "}"
+	return
 }
 
 func (g *Gen) putAtom(a Atom) {
-    if _, b := g.mapping[a.Id()]; !b {
-        g.nextId++
-        id := g.nextId
-        g.mapping[a.Id()] = id
-    }
+	if _, b := g.mapping[a.Id()]; !b {
+		g.nextId++
+		id := g.nextId
+		g.mapping[a.Id()] = id
+	}
 }
 
 func (g *Gen) getId(a Atom) (id int) {
-    id, b := g.mapping[a.Id()]
+	id, b := g.mapping[a.Id()]
 
-    if !b {
-        g.nextId++
-        id = g.nextId
-        g.mapping[a.Id()] = id
-    }
+	if !b {
+		g.nextId++
+		id = g.nextId
+		g.mapping[a.Id()] = id
+	}
 
-    return id
+	return id
 }
 
 func (g *Gen) printSymbolTable(filename string) {
 
-    symbolFile, err := os.Create(filename)
+	symbolFile, err := os.Create(filename)
 
-    if err != nil {
-        panic(err)
-    }
-    // close on exit and check for its returned error
-    defer func() {
-        if err := symbolFile.Close(); err != nil {
-            panic(err)
-        }
-    }()
+	if err != nil {
+		panic(err)
+	}
+	// close on exit and check for its returned error
+	defer func() {
+		if err := symbolFile.Close(); err != nil {
+			panic(err)
+		}
+	}()
 
-    // make a write buffer
-    w := bufio.NewWriter(symbolFile)
+	// make a write buffer
+	w := bufio.NewWriter(symbolFile)
 
-    for i, s := range g.mapping {
-        // write a chunk
-        if _, err := w.Write([]byte(fmt.Sprintln(i, "\t:", s))); err != nil {
-            panic(err)
-        }
-    }
+	for i, s := range g.mapping {
+		// write a chunk
+		if _, err := w.Write([]byte(fmt.Sprintln(i, "\t:", s))); err != nil {
+			panic(err)
+		}
+	}
 
-    if err = w.Flush(); err != nil {
-        panic(err)
-    }
+	if err = w.Flush(); err != nil {
+		panic(err)
+	}
 
 }
--- threshold/pb_test.go
+++ threshold/pb_test.go
@@ -1,83 +1,83 @@
 package threshold
 
 import (
-    "fmt"
-    "github.com/vale1410/bule/sat"
-    "github.com/vale1410/bule/sorters"
-    "math"
-    "strconv"
-    "testing"
+	"fmt"
+	"github.com/vale1410/bule/sat"
+	"github.com/vale1410/bule/sorters"
+	"math"
+	"strconv"
+	"testing"
 )
 
 func TestExample(test *testing.T) {
 
-    filename := "test"
-    //typ := sorters.Bubble
-    typ := sorters.Pairwise
-    //typ := sorters.Bitonic
-    //typ := sorters.OddEven
+	filename := "test"
+	//typ := sorters.Bubble
+	typ := sorters.Pairwise
+	//typ := sorters.Bitonic
+	//typ := sorters.OddEven
 
-    //Example 1
-    //t := createCardinality(4, 15, 5)
-    //t := createCardinality(2, 1, 1)
-    //t := createCardinality(9, 1, 1)
+	//Example 1
+	//t := createCardinality(4, 15, 5)
+	//t := createCardinality(2, 1, 1)
+	//t := createCardinality(9, 1, 1)
 
-    //Example 2
-    //t := createCardinality(8, 4, 1)
-    //t := createCardinality(8,8,2)
-    //t := createCardinality(8,16,4)
-    //filename := "cardinality_8_16_4"
+	//Example 2
+	//t := createCardinality(8, 4, 1)
+	//t := createCardinality(8,8,2)
+	//t := createCardinality(8,16,4)
+	//filename := "cardinality_8_16_4"
 
-    //Example 3
-    //t := createCardinality(8,12,3)
-    //filename := "cardinality_8_12_3"
+	//Example 3
+	//t := createCardinality(8,12,3)
+	//filename := "cardinality_8_12_3"
 
-    //Example 4
-    //t := createExample1()
-    //filename := "example1"
+	//Example 4
+	//t := createExample1()
+	//filename := "example1"
 
-    //Example 5
-    //t := createJapan1(80)
-    //filename := "japan1_10"
+	//Example 5
+	//t := createJapan1(80)
+	//filename := "japan1_10"
 
-    //Example 6
-    //t := createJapan2(3)
-    //filename := "japan2_3"
+	//Example 6
+	//t := createJapan2(3)
+	//filename := "japan2_3"
 
-    t1 := createIgnasi1()
-    t2 := createIgnasi2()
+	t1 := createIgnasi1()
+	t2 := createIgnasi2()
 
-    t1.CreateSorter(typ)
-    t2.CreateSorter(typ)
+	t1.CreateSorter(typ)
+	t2.CreateSorter(typ)
 
-    //fmt.Println(t)
+	//fmt.Println(t)
 
-    PrintThresholdTikZ(filename+".tex", []Threshold{t1, t2})
+	PrintThresholdTikZ(filename+".tex", []Threshold{t1, t2})
 
 }
 
 func TestPBOGeneration(test *testing.T) {
 
-    //t := createJapan1(80)
-    //t := createJapan2(16)
+	//t := createJapan1(80)
+	//t := createJapan2(16)
 
-    //fmt.Printf("* #variable= %v #constraint= %v\n", len(t.Entries), 2)
-    //fmt.Println("****************************************")
-    //fmt.Println("* begin normalizer comments")
-    //fmt.Println("* category= SAT/UNSAT-BIGINT")
-    //fmt.Println("* end normalizer comments")
-    //fmt.Println("****************************************")
+	//fmt.Printf("* #variable= %v #constraint= %v\n", len(t.Entries), 2)
+	//fmt.Println("****************************************")
+	//fmt.Println("* begin normalizer comments")
+	//fmt.Println("* category= SAT/UNSAT-BIGINT")
+	//fmt.Println("* end normalizer comments")
+	//fmt.Println("****************************************")
 
-    //t.Print10()
+	//t.Print10()
 
-    //if t.Typ == AtMost {
-    //  t.Typ = AtLeast
-    //    //t.K++
-    //} else if t.Typ == AtLeast {
-    //  t.Typ = AtMost
-    //    //t.K--
-    //}
-    //t.Print10()
+	//if t.Typ == AtMost {
+	//  t.Typ = AtLeast
+	//    //t.K++
+	//} else if t.Typ == AtLeast {
+	//  t.Typ = AtMost
+	//    //t.K--
+	//}
+	//t.Print10()
 }
 
 //func TestSimple1(test *testing.T) {
@@ -120,137 +120,137 @@ func TestPBOGeneration(test *testing.T) {
 //}
 
 func createEntries(weights []int64) (entries []Entry) {
-    p := sat.Pred("test")
-    entries = make([]Entry, len(weights))
+	p := sat.Pred("x")
+	entries = make([]Entry, len(weights))
 
-    for i := 0; i < len(weights); i++ {
-        l := sat.Literal{true, sat.NewAtomP1(p,i)}
-        entries[i] = Entry{l, weights[i]}
-    }
-    return
+	for i := 0; i < len(weights); i++ {
+		l := sat.Literal{true, sat.NewAtomP1(p, i)}
+		entries[i] = Entry{l, weights[i]}
+	}
+	return
 }
 
 func createExample1() (t Threshold) {
-    weights := []int64{11, 10, 6, 5}
-    t.K = 25
+	weights := []int64{11, 10, 6, 5}
+	t.K = 25
 
-    t.Desc = "Simple Test"
-    t.Typ = AtMost
-    t.Entries = createEntries(weights)
-    return
+	t.Desc = "Simple Test"
+	t.Typ = AtMost
+	t.Entries = createEntries(weights)
+	return
 }
 
 func createExample2() (t Threshold) {
-    weights := []int64{-1, -1, -1, 1}
-    t.K = 0
+	weights := []int64{-1, -1, -1, 1}
+	t.K = 0
 
-    t.Desc = "Simple Test"
-    t.Typ = AtLeast
-    t.Entries = createEntries(weights)
-    return
+	t.Desc = "Simple Test"
+	t.Typ = AtLeast
+	t.Entries = createEntries(weights)
+	return
 }
 
 func createCardinality(n int, k int64, weight int64) (t Threshold) {
 
-    t.K = k
+	t.K = k
 
-    t.Desc = "Cardinality Test"
-    t.Typ = AtMost
-    weights := make([]int64, n)
-    for i := 0; i < n; i++ {
-        weights[i] = weight
-    }
-    t.Entries = createEntries(weights)
+	t.Desc = "Cardinality Test"
+	t.Typ = AtMost
+	weights := make([]int64, n)
+	for i := 0; i < n; i++ {
+		weights[i] = weight
+	}
+	t.Entries = createEntries(weights)
 
-    return
+	return
 }
 
 func createIgnasi1() (t Threshold) {
 
-    weights := []int64{4, 3, 1, 1, 1, 1}
-    t.K = 5
+	weights := []int64{4, 3, 1, 1, 1, 1}
+	t.K = 5
 
-    t.Desc = "Ignasi 1"
-    t.Typ = AtMost
-    t.Entries = createEntries(weights)
-    return
+	t.Desc = "Ignasi 1"
+	t.Typ = AtMost
+	t.Entries = createEntries(weights)
+	return
 }
 
 func createIgnasi2() (t Threshold) {
 
-    weights := []int64{7, 6, 2, 2, 2, 2, 1, 1, 1, 1, 1}
-    t.K = 12
+	weights := []int64{7, 6, 2, 2, 2, 2, 1, 1, 1, 1, 1}
+	t.K = 12
 
-    t.Desc = "Ignasi 2"
-    t.Typ = AtMost
-    t.Entries = createEntries(weights)
-    return
+	t.Desc = "Ignasi 2"
+	t.Typ = AtMost
+	t.Entries = createEntries(weights)
+	return
 }
 
 // creats a Threshold function of size n (n must be even)
 // given in "Size of OBDDs representing threshold functions"
 func createJapan1(n int) (t Threshold) {
 
-    t.Desc = "Japan 1 size " + strconv.Itoa(n)
-    t.Typ = AtLeast
-    //t.Entries = make([]Entry, n)
-    weights := make([]int64, n)
+	t.Desc = "Japan 1 size " + strconv.Itoa(n)
+	t.Typ = AtLeast
+	//t.Entries = make([]Entry, n)
+	weights := make([]int64, n)
 
-    x := int64(1)
+	x := int64(1)
 
-    for i := 0; i < n/2; i++ {
-        e := n - i - 1
-        t.K += x
-        weights[e] = x
-        //t.Entries[e] = Entry{Literal{true, Atom(e + 1)}, x}
-        x = x * 2
-    }
+	for i := 0; i < n/2; i++ {
+		e := n - i - 1
+		t.K += x
+		weights[e] = x
+		//t.Entries[e] = Entry{Literal{true, Atom(e + 1)}, x}
+		x = x * 2
+	}
 
-    y := x / 2
+	y := x / 2
 
-    for i := n / 2; i < n; i++ {
-        e := n - i - 1
-        t.K += x - y
-        weights[e] = x - y
-        //t.Entries[e] = Entry{Literal{true, Atom(e + 1)}, x - y}
-        y = y / 2
-    }
+	for i := n / 2; i < n; i++ {
+		e := n - i - 1
+		t.K += x - y
+		weights[e] = x - y
+		//t.Entries[e] = Entry{Literal{true, Atom(e + 1)}, x - y}
+		y = y / 2
+	}
 
-    t.Entries = createEntries(weights)
-    t.K = t.K / 2
+	t.Entries = createEntries(weights)
+	t.K = t.K / 2
 
-    return
+	return
 }
 
 // creates a Threshold function of size n=k*k
 // given in "Size of OBDDs representing threshold functions"
 func createJapan2(k int) (t Threshold) {
 
-    n := k * k
-    t.Desc = "Japan 2 size " + strconv.Itoa(n)
-    t.Typ = AtLeast
-    //t.Entries = make([]Entry, n)
-    weights := make([]int64, n)
+	n := k * k
+	t.Desc = "Japan 2 size " + strconv.Itoa(n)
+	t.Typ = AtLeast
+	//t.Entries = make([]Entry, n)
+	weights := make([]int64, n)
 
-    x := int64(1)
-    y := int64(math.Exp2(float64(k)))
+	x := int64(1)
+	y := int64(math.Exp2(float64(k)))
 
-    fmt.Println(y)
+	fmt.Println(y)
 
-    for i := 0; i < k; i++ {
-        for j := 0; j < k; j++ {
-            e := n - i*k - j - 1
-            t.K += x + y
-            weights[e] = x + y
-            //t.Entries[e] = Entry{Literal{true, Atom(e + 1)}, x + y}
-            x = x * 2
-        }
-        y = y * 2
-        x = int64(1)
-    }
+	for i := 0; i < k; i++ {
+		for j := 0; j < k; j++ {
+			e := n - i*k - j - 1
+			t.K += x + y
+			weights[e] = x + y
+			//t.Entries[e] = Entry{Literal{true, Atom(e + 1)}, x + y}
+			x = x * 2
+		}
+		y = y * 2
+		x = int64(1)
+	}
 
-    t.Entries = createEntries(weights)
-    t.K = t.K / 2
+	t.Entries = createEntries(weights)
+	t.K = t.K / 2
 
-    return
+	return
 }
