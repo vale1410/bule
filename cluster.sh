a41966c5102578c70b2c996921db1ea02a4c3cca
Valentin Mayer-Eichberger <veichberger@cluster2.it.nicta.com.au>
2015-04-10 22:37:12 +1000

    new experiement:

--- cluster.sh
+++ cluster.sh
@@ -3,28 +3,38 @@
 timeout=1800
 
 #go build; 
-results=results
-bound=55
+results=results2
 rm -fr $results
 mkdir -p $results
 
-
-for x in $1/*.pb; 
-do 
-    echo ./bule -cat 1 -opt-bound $bound -solve -timeout $timeout -solver=minisat -f $x" | grep 'xxx:' >> "$results/$(basename $x .pb)-cat-1-opt-0-amo-0.log
-done 
-
-for amo in 0 1
-do 
-    for opt in 0 1 
-    do 
-    	for same in 0 1 
-    	do 
-        	for x in $1/*.pb; 
-        	do 
-		echo ./bule -cat 2 -opt-bound $bound -rewrite-same=$same -solve -amo-reuse=$amo -opt-rewrite=$opt -timeout $timeout -solver=minisat -f $x" | grep 'xxx:' >> "$results/$(basename $x .pb)-cat-2-opt-$opt-amo-$amo-same-$same-bound-$bound.log
-        	done 
-    	done 
-    done 
+for amo in 0 1 
+do
+	for same in 0 1 
+	do
+		for x in $1/*.pb; 
+		do 
+		echo ./bule -d -cat 2 -rewrite-same=$same -solve -amo-chain=$amo -amo-reuse=1 -timeout $timeout -f $x" > "$results/$(basename $x .pb)-amo-$amo-same-$same.log
+		done 
+	done 
 done
 
+
+#for x in $1/*.pb; 
+#do 
+#    echo ./bule -cat 1 -opt-bound $bound -solve -timeout $timeout -solver=minisat -f $x" | grep 'xxx:' >> "$results/$(basename $x .pb)-cat-1-opt-0-amo-0.log
+#done 
+#
+#for amo in 0 1
+#do 
+#    for opt in 0 1 
+#    do 
+#    	for same in 0 1 
+#    	do 
+#        	for x in $1/*.pb; 
+#        	do 
+#		echo ./bule -cat 2 -opt-bound $bound -rewrite-same=$same -solve -amo-reuse=$amo -opt-rewrite=$opt -timeout $timeout -solver=minisat -f $x" | grep 'xxx:' >> "$results/$(basename $x .pb)-cat-2-opt-$opt-amo-$amo-same-$same-bound-$bound.log
+#        	done 
+#    	done 
+#    done 
+#done
+#
