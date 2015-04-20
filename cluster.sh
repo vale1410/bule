0c9ca6a0f0f196cd2f665289f5bf04c1fbcaafc1
vale <vale1410@gmail.com>
2015-04-20 23:20:55 +1000

    experiment ready

--- cluster.sh
+++ cluster.sh
@@ -1,40 +1,29 @@
 #!/bin/zsh
 
-timeout=1800
 
 #go build; 
-results=results2
+results=results
 rm -fr $results
 mkdir -p $results
 
-for amo in 0 1 
-do
-	for same in 0 1 
-	do
-		for x in $1/*.pb; 
-		do 
-		echo ./bule -d -cat 2 -rewrite-same=$same -solve -amo-chain=$amo -amo-reuse=1 -timeout $timeout -f $x" > "$results/$(basename $x .pb)-amo-$amo-same-$same.log
-		done 
-	done 
-done
+for timeout in 300 900 3600
+do 
+    for seed in  42 132 6534 7654 3456 
+    do 
 
-
-#for x in $1/*.pb; 
-#do 
-#    echo ./bule -cat 1 -opt-bound $bound -solve -timeout $timeout -solver=minisat -f $x" | grep 'xxx:' >> "$results/$(basename $x .pb)-cat-1-opt-0-amo-0.log
-#done 
-#
-#for amo in 0 1
-#do 
-#    for opt in 0 1 
-#    do 
-#    	for same in 0 1 
-#    	do 
-#        	for x in $1/*.pb; 
-#        	do 
-#		echo ./bule -cat 2 -opt-bound $bound -rewrite-same=$same -solve -amo-reuse=$amo -opt-rewrite=$opt -timeout $timeout -solver=minisat -f $x" | grep 'xxx:' >> "$results/$(basename $x .pb)-cat-2-opt-$opt-amo-$amo-same-$same-bound-$bound.log
-#        	done 
-#    	done 
-#    done 
-#done
-#
+        for amo in 0 1 
+        do
+            for x in $1/*.pb; 
+            do 
+            echo ./bule -d -amo-chain=$amo -timeout $timeout -f $x -seed $seed " > "$results/$(basename $x .pb)-amo-$amo-$seed-$timeout.log
+            done 
+        done
+        
+        for x in $1/*.pb
+        do 
+            echo ./bule -f $x -gringo '| gringo | clasp --time-limit' $timeout --seed $seed' > '$results/$(basename $x .pb)-clasp-$seed-$timeout.log
+        done 
+    done 
+done 
+    
+    
