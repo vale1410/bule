84463f51a77434b3ec3b0fc084b0b744d90c1910
Valentin Mayer-Eichberger <vale1410@gmail.com>
2020-07-06 03:18:36 +0200

    more texte

--- cmd/solve.go
+++ cmd/solve.go
@@ -39,13 +39,10 @@ import (
 // solveCmd represents the solve command
 var solveCmd = &cobra.Command{
 	Use:   "solve",
-	Short: "A brief description of your command",
+	Short: "Grounds the bule formula and passes it to DEPQBF, then it outputs a model if it exists. ",
 	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+and usage of using your command.
+`,
 	Run: func(cmd *cobra.Command, args []string) {
 
 		if len(args) == 0 {
