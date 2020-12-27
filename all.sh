dcc1e5d80b940ded046a4c1f9598e395ccadd57c
Valentin Mayer-Eichberger <vale1410@gmail.com>
2020-12-27 02:42:12 +0100

    Separators in Bule script.

--- all.sh
+++ all.sh
@@ -14,13 +14,20 @@ do
     ./main ground  -t=1 -u=0 -i=1 -q=0 --facts $x 2>/dev/null | sed 's/%.*//g' | sed '/^\s*$/d' | sort > test-output/$name 
     diff -B -b test-expected/$name test-output/$name > /dev/null
     if [ $? -ne 0 ] ; then 
+        echo ---------
         echo test failed: $name
         echo input: 
+        echo ---------
         cat test-input/$name
+        echo ---------
         echo output: 
+        echo ---------
         cat test-output/$name
+        echo ---------
         echo expected: 
+        echo ---------
         cat test-expected/$name
+        echo ---------
     else 
         echo  ☑ $name 
     fi 
