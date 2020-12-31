15ede1b1185b2565d1ad3600be2d7ad6a54b3d78
Valentin Mayer-Eichberger <vale1410@gmail.com>
2020-12-31 02:52:43 +0100

    Fix script

--- test-pb.sh
+++ test-pb.sh
@@ -5,11 +5,12 @@ make
 echo GRINGO
 for x in instances/*/*; do echo $x $(./bule pb -f $x --gringo --solve=false | gringo | clasp | grep 'SATIS\|Optimization \|OPTIMUM FOUND'); done
 echo 
-echo BULE with cat 1 and cmsat
+
+echo BULE with cat 1 
 for x in instances/*/*; 
-do 
-    ./bule pb -f $x --cat 1 --solver clasp | grep instance; done | column -s ';' -t 
-    ./bule pb -f $x --cat 1 --solver clasp | grep instance; done | column -s ';' -t 
+do ./bule pb -f $x --cat 1 --solver clasp | grep instance; done | column -s ';' -t 
 echo 
-echo BULE with cat 2 and cmsat
-for x in instances/*/*; do ./bule pb -f $x --cat 2 --solver clasp| grep instance; done | column -s ';' -t 
+
+echo BULE with cat 2 
+for x in instances/*/*; 
+do ./bule pb -f $x --cat 2 --solver clasp| grep instance; done | column -s ';' -t 
