a4538db4e39680ea4d0f3d2255d331e180fc1e04
Abdallah Saffidine <abdallah.saffidine@gmail.com>
2021-05-12 22:09:18 +1000

    Update connect.bul to have even-odd board times.

--- examples/connect4/all_connect.sh
+++ examples/connect4/all_connect.sh
@@ -26,16 +26,16 @@ output[1]=UNSAT
 input[2]='q=2,c=2,r=2,d=1'
 output[2]=UNSAT
 
-input[3]='q=2,c=2,r=2,d=2'
+input[3]='q=2,c=2,r=2,d=3'
 output[3]=SAT
 
-input[4]='q=3,c=3,r=3,d=3'
+input[4]='q=3,c=3,r=3,d=5'
 output[4]=UNSAT
 
-input[5]='q=3,c=3,r=3,d=4'
+input[5]='q=3,c=3,r=3,d=7'
 output[5]=UNSAT
 
-input[6]='q=3,c=3,r=3,d=5'
+input[6]='q=3,c=3,r=3,d=9'
 output[6]=UNSAT
 
 
