b5fcab3c99361ba74c856fa3a85fb99968411b79
Abdallah Saffidine <abdallah.saffidine@gmail.com>
2021-05-13 16:53:35 +1000

    Fix computation of d for connect4 benchmarks.

--- examples/connect4/make_c4_bench.py
+++ examples/connect4/make_c4_bench.py
@@ -4,11 +4,10 @@ import sys
 
 def process_line(moves, target):
     assert target != 0
-    firstPlayerWin = target > 0
-    if firstPlayerWin:
-        neededForAWin = 43-len(moves)-2*target
-    else:
-        neededForAWin = 43-len(moves)+2*target
+    firstPlayerWins = target > 0
+    startingPWins = (len(moves) % 2 == 0) == firstPlayerWins
+    winLength = 44 - 2*abs(target) - (1 if startingPWins else 0)
+    remainingMoves = winLength-len(moves)
     heights = [1] * 7
     startingP = True
     inits = []
@@ -16,8 +15,7 @@ def process_line(moves, target):
         inits.append((startingP,move,heights[move-1]))
         heights[move-1] += 1
         startingP = not startingP
-    output = "#const q=4.\n#const c=7.\n#const r=6.\n#const d={}.\n".format(neededForAWin)
-    startingPWins = (len(moves) % 2 == 0) == firstPlayerWin
+    output = "#const q=4.\n#const c=7.\n#const r=6.\n#const d={}.\n".format(remainingMoves)
     for (b, c, r) in inits:
         p = "winner" if b == startingPWins else "loser"
         output += "init[{},{},{}].\n".format(p, c, r)
