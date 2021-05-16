2182deffa8af925b926baa51e9335a38b780bc5c
Valentin Mayer-Eichberger <vale1410@gmail.com>
2021-05-16 14:15:48 +0200

    automated release generation

--- cmd/version.go
+++ cmd/version.go
@@ -38,7 +38,7 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Bule Version 3.0.2 Copyright Valentin Mayer-Eichberger, 2021-05-13")
+		fmt.Println("Bule Version {{{VERSION}}} Copyright Valentin Mayer-Eichberger {{{DATE}}}")
 		return
 	},
 }
--- make_release.sh
+++ make_release.sh
@@ -1,7 +1,16 @@
 #!/bin/zsh
 
-RE=release-$(git describe --tags)
+TAG=$(git describe --tags)
+
+echo preparing a release with tag $TAG
+
+RE=release-$TAG
 mkdir $RE
+
+### insert git tag in cmd/version.go ; it is set back afterwards
+mv cmd/version.go $RE/version.go
+DATES=$(date "+%Y-%m-%d") 
+cat $RE/version.go | sed s/{{{VERSION}}}/$TAG/g | sed s/{{{DATE}}}/$DATES/g > cmd/version.go
 
 env GOOS=linux GOARCH=amd64 go build main.go
 mv main $RE/bule_x64
@@ -9,3 +18,6 @@ env GOOS=darwin GOARCH=amd64 go build main.go
 mv main $RE/bule_mac64
 env GOOS=windows GOARCH=amd64 go build main.go
 mv main.exe $RE/bule_win64.exe
+
+### Put version back
+mv $RE/version.go cmd/version.go 
