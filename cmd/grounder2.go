ceffbce6db1d17559161045176a946973d180da0
Valentin Mayer-Eichberger <vale1410@gmail.com>
2021-07-05 23:00:49 +0200

    reformat

--- cmd/grounder2.go
+++ cmd/grounder2.go
@@ -4,16 +4,16 @@ import bule "github.com/vale1410/bule/grounder"
 
 func stageX1GeneratorsAndFacts(p *bule.Program) {
 
-//	// COMPUTE ORDER IN WHICH TO PROCESS
-//	// Stage 1: collect tuples for g-lits and s-lits
-//	var order []*bule.Rule
-//	for _, r := range order {
-//		// until fixpoint:
-//		// - roll out g-literal
-//		// - clean up instantiations
-//		// - potentially remove rules
-//		// if head is ground -> add to tuples.
-//	}
+	//	// COMPUTE ORDER IN WHICH TO PROCESS
+	//	// Stage 1: collect tuples for g-lits and s-lits
+	//	var order []*bule.Rule
+	//	for _, r := range order {
+	//		// until fixpoint:
+	//		// - roll out g-literal
+	//		// - clean up instantiations
+	//		// - potentially remove rules
+	//		// if head is ground -> add to tuples.
+	//	}
 
 }
 
